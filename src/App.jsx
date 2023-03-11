d816e93f0783238758f790832c8fe36e8f884970
vignesh-228 <vignesh.bondla228@gmail.com>
2023-03-11 13:24:33 +0530

    Added register form

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,13 @@
-
-import './App.css'
-import Login from './components/Login'
+import "./App.css";
+import Login from "./Components/Login";
+import Register from "./Components/Register";
 
 function App() {
-
-
   return (
     <div className="App">
-      <Login />
+      <Register />
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
--- src/components/Register.jsx
+++ src/components/Register.jsx
@@ -1,5 +1,32 @@
+import "./Register.css";
+
 function Register() {
-  return <h1>register component</h1>;
+  return (
+    <div id="main-container">
+      <form>
+        <label htmlFor="text">Username</label>
+        <input
+          type="text"
+          name="uname"
+          placeholder="Enter your username"
+          required
+        />{" "}
+        <br />
+        <label htmlFor="password">Password</label>
+        <input
+          type="password"
+          name="password"
+          placeholder="Enter your password"
+          required
+        />
+        <br />
+        <label htmlFor="text">Roll</label>
+        <input type="text" name="roll" placeholder="Enter your roll" required />
+        <br />
+        <button type="button">Register</button>
+      </form>
+    </div>
+  );
 }
 
 export default Register;
